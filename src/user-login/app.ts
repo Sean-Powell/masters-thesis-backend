b90402f5f8ebcb09a69cb08a33e327c14d10db87
Sean <seanpowell54@gmail.com>
2024-03-17 12:55:13 +0100

    removed debug logs

--- src/user-login/app.ts
+++ src/user-login/app.ts
@@ -4,8 +4,5 @@ export const handler = async (event) => {
     const requestBody = JSON.parse(event.body);
     const email: string = requestBody.email;
     const password: string = requestBody.password;
-    //TODO remove these debug logs
-    console.info("email : " + email);
-    console.info("password: " + password);
     return await AuthenticateUser(email, password);
 }
